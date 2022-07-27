49d34717e60eca10bc86a6a0b91981ed37e0dcb5
Jacopo Tessera <jacopotessera@gmail.com>
2022-07-27 23:15:10 +0200

    vault

---
+++ configure.vault.sh
@@ -0,0 +1,5 @@
+export ARCH_INSTALL_CONFIGURATION=configure.vault.sh
+export ARCH_INSTALL_DISK='sda'
+export ARCH_INSTALL_P=''
+export ARCH_INSTALL_HOSTNAME='vault'
+export ARCH_INSTALL_USER='vault'
